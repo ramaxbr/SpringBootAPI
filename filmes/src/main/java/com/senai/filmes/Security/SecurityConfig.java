4c325a2bc92fc8b672204683450386a537984b0d
Turma Fullstask <fullstack@info.local>
2026-06-12 21:42:09 -0300

    fix adicionando uma resolução ao packet SecurityConfig

--- filmes/src/main/java/com/senai/filmes/Security/SecurityConfig.java
+++ filmes/src/main/java/com/senai/filmes/Security/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -60,12 +61,19 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource(){
-        CorsConfigurationSource config = new CorsConfiguration();
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(List.of(""))
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+
     }
 
     @Bean
